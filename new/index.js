46944eb6620bf4ae6337676ee3f63970cf7c2789
Miracle Ohuka <miralexohuka12@gmail.com>
2024-04-25 00:51:51 +0100

    lets deal with arrays in javascript

--- new/index.js
+++ new/index.js
@@ -1,3 +1,16 @@
-numbers = [1, 2, 3, 4, 5];
-let maxnum = Math.min(...numbers);
-console.log(maxnum);
+function rollDice() {
+  const numOfDice = document.getElementById("numberofdice").value;
+  const diceResult = document.getElementById("diceResult");
+  const diceImages = document.getElementById("diceImages");
+  const values = [];
+  const images = [];
+
+  for (i = 0; i < numOfDice; i++) {
+    let result = Math.floor(Math.random() * 6) + 1;
+    values.push(result);
+    images.push(`<img src="images/dice${result}.jpeg">`);
+  }
+
+  diceResult.textContent = `dice: ${values.join(", ")}`;
+  diceImages.innerHTML = images.join("");
+}
